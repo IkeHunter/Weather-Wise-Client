14576eef73605db365915c5cce1d5870c405420f
Isaac Hunter <ikehunter5@gmail.com>
2023-04-27 03:20:10 -0400

    configure postalcode form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -74,7 +75,8 @@ import { WidgetTitlePipe } from './pipes/widgets.pipe';
         AppRoutingModule,
         HttpClientModule,
         LottieModule,
-        FormsModule
+        FormsModule,
+        ReactiveFormsModule
     ]
 })
 export class AppModule { }
--- src/app/modules/dashboard/location/location.component.ts
+++ src/app/modules/dashboard/location/location.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, NgForm } from '@angular/forms';
 import { Summary } from 'src/app/models/summary.model';
 
 @Component({
@@ -8,15 +9,26 @@ import { Summary } from 'src/app/models/summary.model';
 })
 export class LocationComponent implements OnChanges {
   @Input() summary: Summary;
+  @ViewChild('f') searchForm: NgForm;
 
   cityName: string = "";
   postalCode: number = 0;
+  newPostalCode: number = 0;
+
+  locationForm = new FormGroup({
+    postalCode: new FormControl('')
+  })
 
   ngOnChanges(changes: SimpleChanges) {
     if(this.summary != undefined) {
       this.cityName = this.summary.city;
       this.postalCode = this.summary.location;
     }
+  }
+
+  onSubmit() {
+    console.log("submit");
+    console.log(this.locationForm.value.postalCode)
   }
 
 }
